987a8e2a5570046c190ae2bebda7246c84724e88
DanKneeD <daniel.dousek@strv.com>
2021-08-16 12:51:11 +0200

    More efficent

--- src/App.js
+++ src/App.js
@@ -7,12 +7,13 @@ import Footer from "./components/Footer";
 import { useState, useEffect } from "react";
 
 function App() {
-  const width = window.innerWidth;
-  const numRow = Math.floor(width / 10);
+  const numRow = Math.floor(window.innerWidth / 11);
+  const [runningAlgorithm, setRunningAlgorithm] = useState(false);
   const [rows, setRows] = useState([]);
-  let copy = [];
+  const [arrayAccessCounter, setArrayAccessCounter] = useState(0);
+  const [comparisonCounter, setComparisonCounter] = useState(0);
 
-  const sort = () => {
+  const randomize = () => {
     const tempArr = [];
     for (var i = 0; i < numRow; i++) {
       tempArr.push({
@@ -20,81 +21,106 @@ function App() {
         id: i,
       });
     }
-
     setRows((rows) => tempArr);
+  };
+
+  const changeRunningAlgorithm = (running) => {
+    if (running) {
+      setRunningAlgorithm(true);
+    } else {
+      setRunningAlgorithm(false);
+    }
   };
 
   const bubbleSort = () => {
     let tempArr = [...rows];
-    let i = 0;
+    let tempNum = 0;
+    let tempArrCount = 0;
+
+    changeRunningAlgorithm(true);
 
     var innerLoop = () => {
+      //inner loop
       for (let y = 1; y < numRow; y++) {
-        //inner loop
         if (tempArr[y].height < tempArr[y - 1].height) {
           //swap if height is bigger
-          copy = [...tempArr];
+          let copy = [...tempArr];
           [copy[y - 1], copy[y]] = [copy[y], copy[y - 1]];
-
           tempArr = copy;
+          tempArrCount++;
         }
+        tempNum++;
+        tempArrCount = tempArrCount + 2;
       }
-      setRows(tempArr); //add to State
+      //add to States
+      setRows(tempArr);
+      setComparisonCounter(tempNum);
+      setArrayAccessCounter(tempArrCount);
     };
 
     //outer loop
-    for (i = 0; i < numRow; i++) {
+    for (let i = 0; i < numRow; i++) {
       setTimeout(innerLoop, 50 * i);
+      setTimeout(() => {
+        changeRunningAlgorithm(false);
+      }, 50 * numRow);
     }
   };
 
   const selectionSort = () => {
+    let tempNumCount = 0;
+    let tempArrCount = 0;
     let tempArr = [...rows];
     let minVal = 0;
-    let y = 0;
-    let i = 0;
     let x = 0;
+
+    changeRunningAlgorithm(true); //disable button
 
     var innerLoop = () => {
       minVal = x;
 
-      for (i = 1 + x; i < numRow; i++) {
+      for (let i = 1 + x; i < numRow; i++) {
         if (tempArr[i].height < tempArr[minVal].height) {
           minVal = i;
         }
+        tempArrCount = tempArrCount + 2;
+        tempNumCount++;
       }
 
       //swapping array positions
       let data = [...tempArr];
       let temp = [data[x]];
-
       [data[x]] = [data[minVal]];
       [data[minVal]] = temp;
-
       tempArr = data;
 
-      //adding new array to DOM
+      //adding new states to DOM
+      tempArrCount = tempArrCount + 3;
       setRows(tempArr);
+      setArrayAccessCounter(tempArrCount);
+      setComparisonCounter(tempNumCount);
       x++;
     };
-    for (y = 0; y < numRow; y++) {
+
+    //outerloop
+    for (let y = 0; y < numRow; y++) {
       setTimeout(innerLoop, 50 * y);
+      setTimeout(() => {
+        changeRunningAlgorithm(false);
+      }, 50 * numRow);
     }
   };
-
-  /*useEffect(() => {
-    //console.log("changed");
-   
-  }, [rows]);*/
 
   return (
     <div className='App'>
       <Header />
       <DisplayAlgo rows={rows} />
       <Footer
-        sort={sort}
-        bubbleSort={bubbleSort}
-        selectionSort={selectionSort}
+        sort={runningAlgorithm ? undefined : randomize}
+        bubbleSort={runningAlgorithm ? undefined : bubbleSort}
+        selectionSort={runningAlgorithm ? undefined : selectionSort}
+        displayNum={comparisonCounter}
+        displayArrayCount={arrayAccessCounter}
       />
     </div>
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,19 @@
-const Footer = ({ sort, bubbleSort, selectionSort }) => {
+const Footer = ({
+  sort,
+  bubbleSort,
+  selectionSort,
+  displayNum,
+  displayArrayCount,
+}) => {
   return (
     <div className='Footer'>
-      <button className='red' onClick={sort}>
-        Randomize{" "}
-      </button>
+      <button onClick={sort}>Randomize </button>
       <button onClick={bubbleSort}> Bubble Sort</button>
       <button onClick={selectionSort}>Selection Sort</button>
+      <div className='displayDiv'> Number of comparisons {displayNum}</div>
+      <div className='displayDiv'>
+        Number of Access's to Array {displayArrayCount}
+      </div>
     </div>
   );
 };
